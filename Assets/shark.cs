ea24835b856cf6e2cbeb91150f43cdb47bfe0adc
ezrarezku-prog <ezrarezku@gmail.com>
2026-02-10 21:29:40 +0100

    Revert "Merge branch 'moving'"
    This reverts commit dbc4c531ade911c3adda353f6523d6a70152a838, reversing
    changes made to ef71b4758cc79065de668b8682628b01e1fa0d6a.

--- Assets/shark.cs
+++ Assets/shark.cs
@@ -1,11 +1,7 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class shark : MonoBehaviour
 {
-    [SerializeField] float turnSpeed = 150f;
-    [SerializeField] float moveSpeed = 5f;
     
     void Start()
     {
@@ -15,33 +11,6 @@ public class shark : MonoBehaviour
     
     void Update()
     {
-        float move = 0f;
-        float turn = 0f;
-    
-
-        if (Keyboard.current.wKey.isPressed)
-        {
-            move = 1f;
-        }
-        else if (Keyboard.current.sKey.isPressed)
-        {
-            move = -1f;
-        }
-
-        if (Keyboard.current.aKey.isPressed)
-        {
-            turn = 1f;
-        }
-        else if (Keyboard.current.dKey.isPressed)
-        {
-            turn = -1f;
-        }
-    
-        float moveAmount = move * moveSpeed * Time.deltaTime;
-        float turnAmount = turn * turnSpeed * Time.deltaTime;
-
-        transform.Rotate(0, 0, turnAmount);
-        transform.Translate(0, moveAmount, 0);
-        
+        transform.Rotate(0, 0, 0.02f);
     }
 }
