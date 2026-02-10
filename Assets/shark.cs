f52b8ca4d0bfe1a64c09fb32115ff9f49f53a9a0
ezrarezku-prog <ezrarezku@gmail.com>
2026-02-10 16:09:38 +0100

    test transform

---
+++ Assets/shark.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class shark : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(0, 0, 0.02f);
+    }
+}
